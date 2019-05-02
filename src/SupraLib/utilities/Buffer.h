471769f32b958915293117b198c930dcef8fcb86
Ruediger Goebl <r.goebl@tum.de>
2019-05-02 22:54:11 +0200

    Added cachedBuffer2

--- src/SupraLib/utilities/Buffer.h
+++ src/SupraLib/utilities/Buffer.h
@@ -34,10 +34,15 @@ namespace supra
 
 		__device__ __host__ ElementType& operator[](vec2T<IndexType> index)
 		{
-			return m_buffer[index.x + index.y * m_size.x];
+			return accessBuffer(index);
 		}
 
 	protected:
+		__device__ __host__ ElementType& accessBuffer(vec2T<IndexType> index)
+		{
+			return m_buffer[index.x + index.y * m_size.x];
+		}
+
 		ElementPtrType m_buffer;
 		vec2T<IndexType> m_size;
 	};
@@ -70,6 +75,87 @@ namespace supra
 		vec3T<IndexType> m_size;
 	};
 
+	template <typename ElementPtrType, typename IndexType>
+	class CachedBuffer2 : Buffer2<ElementPtrType, IndexType>
+	{
+	public:
+		typedef typename std::remove_const<std::remove_pointer<ElementPtrType>::type>::type ModifiableElementType;
+		typedef typename std::add_pointer<ModifiableElementType>::type ModifiableElementPtrType;
+
+	public:
+		__device__ CachedBuffer2(
+			ElementPtrType buffer, vec2T<IndexType> size,
+			ModifiableElementPtrType cacheBuffer, vec2T<IndexType> cacheSize,
+			vec2T<IndexType> cacheOffset)
+			: Buffer2(buffer, size)
+			, m_cachedBuffer{ cacheBuffer }
+			, m_cacheSize{ cacheSize }
+			, m_cacheOffset{ cacheOffset }
+			, m_cacheEnd{ cacheOffset + cacheSize }
+		{
+			loadIntoCache();
+		};
+
+		__device__ ElementType& operator[](vec2T<IndexType> index)
+		{
+			// If the element is cached give the caller access to the cache
+			if (index.x >= m_cacheOffset.x && index.x < m_cacheEnd.x &&
+				index.y >= m_cacheOffset.y && index.y < m_cacheEnd.y)
+			{
+				return accessCache(index);
+			}
+			else // If the element is not cached, refer to the uncached buffer
+			{
+				return accessBuffer(index);
+			}
+		}
+
+		__device__ void writeCacheToBuffer()
+		{
+			// Copy the data in a grid stride loop
+			// ( because the cache usually is bigger than the block)
+			vec2T<IndexType> index{ m_cacheOffset };
+			for (; index.y < m_cacheEnd.y; index.y += blockDim.y)
+			{
+				for (; index.x < m_cacheEnd.x; index.x += blockDim.x)
+				{
+					accessBuffer(index) = accessCache(index);
+				}
+			}
+			__syncthreads();
+		};
+
+	protected:
+
+		__device__ ModifiableElementType& accessCache(vec2T<IndexType> index)
+		{
+			return m_cachedBuffer[
+				(index.x - m_cacheOffset.x) +
+					(index.y - m_cacheOffset.y) * m_cacheSize.x];
+		}
+
+		__device__ void loadIntoCache()
+		{
+			// Copy the data in a grid stride loop
+			// ( because the cache usually is bigger than the block)
+			vec2T<IndexType> index{ m_cacheOffset + vec2T<IndexType>{threadIdx.x, threadIdx.y} };
+			
+			for (; index.y < m_cacheEnd.y; index.y += blockDim.y)
+			{
+				for (; index.x < m_cacheEnd.x; index.x += blockDim.x)
+				{
+					accessCache(index) = accessBuffer(index);
+				}
+			}
+			__syncthreads();
+		};
+
+		ModifiableElementPtrType m_cachedBuffer;
+		vec2T<IndexType> m_cacheSize;
+		vec2T<IndexType> m_cacheOffset;
+		vec2T<IndexType> m_cacheEnd;
+	};
+	
 	template <typename ElementPtrType, typename IndexType>
 	class CachedBuffer3 : Buffer3<ElementPtrType, IndexType>
 	{
