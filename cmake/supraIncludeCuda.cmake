1912a7ed849f0e4619fdbda3be162b600a3c33c3
Ruediger Goebl <r.goebl@tum.de>
2019-05-20 23:23:51 +0200

    Added cuda cmake files for easy support of older platforms (before cmake 3.10)

--- cmake/supraIncludeCuda.cmake
+++ cmake/supraIncludeCuda.cmake
@@ -1,4 +1,9 @@
-find_package(CUDA REQUIRED)
+if(${CMAKE_VERSION} VERSION_LESS "3.10.0") 
+    find_package(CUDA_old REQUIRED)
+else()
+	find_package(CUDA REQUIRED)
+endif()
+
 IF(CUDA_VERSION_MAJOR LESS 10)
 	MESSAGE(FATAL_ERROR "CUDA >= 10.0 is required, but only found ${CUDA_VERSION}. Verify installed cuda toolkit and modify CUDA_TOOLKIT_ROOT_DIR if necessary.")
 ENDIF()
